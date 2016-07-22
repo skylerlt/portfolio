0009b595029fe913cc26df14c837b244f5fdbc19
Skyler Talbot <skylerltalbot84@gmail.com>
2016-07-22 11:40:29 -0600

    added dh loder

---
+++ dh-loder/app.js
@@ -0,0 +1,41 @@
+var app = angular.module("mainApp", ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+
+    $routeProvider
+        .when("/", {
+            templateUrl: "/pages/home.html"
+        })
+        .when("/home", {
+            templateUrl: "/pages/home.html"
+        })
+        .when("/company", {
+            templateUrl: "/pages/company.html"
+        })
+        .when("/contact", {
+            templateUrl: "/pages/contact.html"
+        })
+        .when("/residential", {
+            templateUrl: "/pages/residential.html"
+        })
+        .when("/employ", {
+            templateUrl: "/pages/employment.html"
+        })
+        .when("/clients", {
+            templateUrl: "/pages/clients.html"
+        })
+        .when("/commercial", {
+            templateUrl: "/pages/commercial.html"
+        })
+
+});
+
+app.controller("MainController", ["$scope", function ($scope) {
+
+    $scope.thumb = "house";
+
+}])
+
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').focus()
+})
